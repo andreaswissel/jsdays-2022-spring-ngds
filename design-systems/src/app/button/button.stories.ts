d07d33e7b80ad3d8b66bf6baabe9055c1e607509
Andreas Wissel <github@kaeku.io>
2022-03-22 22:32:17 +0100

    lab-2

---
+++ design-systems/src/app/button/button.stories.ts
@@ -0,0 +1,17 @@
+import { ButtonComponent } from './button.component';
+import { Story } from '@storybook/angular';
+
+const Template: Story<ButtonComponent> = (args) => ({
+  props: args,
+});
+
+export default {
+  title: 'Components/Button',
+  component: ButtonComponent,
+};
+
+export const DefaultButton = Template.bind({});
+
+DefaultButton.args = {
+  label: 'Submit',
+};
